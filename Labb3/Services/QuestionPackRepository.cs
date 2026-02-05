c33bd5b0e7aba6af95b4dfce3812e1ff7b5307cc
Kevin <Kevin@DESKTOP-TE4QL2A>
2026-02-05 20:03:27 +0100

    Implemented full CRUD for QuestionPack in QuestionPackRepository

--- Labb3/Services/QuestionPackRepository.cs
+++ Labb3/Services/QuestionPackRepository.cs
@@ -11,19 +11,41 @@ namespace Labb3.Services
 
         public QuestionPackRepository()
         {
-            
             var client = new MongoClient("mongodb://localhost:27017");
-
-            
             var database = client.GetDatabase("KevinKongpachith");
-
-           
             _collection = database.GetCollection<QuestionPack>("QuestionPacks");
         }
 
         public List<QuestionPack> GetAll()
         {
             return _collection.Find(_ => true).ToList();
+        }
+
+
+        public void Create(QuestionPack pack)
+        {
+            _collection.InsertOne(pack);
+        }
+
+   
+        public void Update(QuestionPack pack)
+        {
+            var filter = Builders<QuestionPack>.Filter.Eq(p => p.Id, pack.Id);
+            _collection.ReplaceOne(filter, pack);
+        }
+
+   
+        public void Delete(ObjectId id)
+        {
+            var filter = Builders<QuestionPack>.Filter.Eq(p => p.Id, id);
+            _collection.DeleteOne(filter);
+        }
+
+     
+        public QuestionPack GetById(ObjectId id)
+        {
+            var filter = Builders<QuestionPack>.Filter.Eq(p => p.Id, id);
+            return _collection.Find(filter).FirstOrDefault();
         }
     }
 }
