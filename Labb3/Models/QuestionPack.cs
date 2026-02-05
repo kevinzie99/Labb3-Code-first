dec37a14d8aa0bfb3ca891c7b40e24244a101311
Kevin <Kevin@DESKTOP-TE4QL2A>
2026-02-05 19:44:06 +0100

    Added ObjectId and category relation to QuestionPack

--- Labb3/Models/QuestionPack.cs
+++ Labb3/Models/QuestionPack.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using System.Collections.Generic;
+
 namespace Labb3.Models;
 
 internal enum Difficulty { Easy, Medium, Hard }
+
 internal class QuestionPack
 {
+    [BsonId]
+    public ObjectId Id { get; set; }
+
+    public string Name { get; set; }
+    public Difficulty Difficulty { get; set; }
+    public int TimeLimitInSeconds { get; set; }
+
+    // Relation till Category
+    public ObjectId CategoryId { get; set; }
+
+    public List<Question> Questions { get; set; }
+
+    // Krävs av MongoDB
+    public QuestionPack()
+    {
+        Questions = new List<Question>();
+    }
 
     public QuestionPack(string name, Difficulty difficulty = Difficulty.Medium, int timeLimitInSeconds = 30)
     {
@@ -13,11 +33,4 @@ internal class QuestionPack
         TimeLimitInSeconds = timeLimitInSeconds;
         Questions = new List<Question>();
     }
-
-    public string Name { get; set; }
-    public Difficulty Difficulty { get; set; }
-    public int TimeLimitInSeconds { get; set; }
-
-    public List<Question> Questions { get; set; }
-
 }
