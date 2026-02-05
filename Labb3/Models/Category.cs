71dd6d2ff40dd2e5d7d41dd9984159a8c5d081ce
Kevin <Kevin@DESKTOP-TE4QL2A>
2026-02-05 20:08:20 +0100

    Added Category model and CategoryRepository with basic CRUD

---
+++ Labb3/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Labb3.Models
+{
+    internal class Category
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        public string Name { get; set; }
+
+        
+        public Category() { }
+
+        public Category(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Labb3/Services/CategoryRepository.cs
@@ -0,0 +1,45 @@
+﻿using Labb3.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections.Generic;
+
+namespace Labb3.Services
+{
+    internal class CategoryRepository
+    {
+        private readonly IMongoCollection<Category> _collection;
+
+        public CategoryRepository()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("KevinKongpachith");
+
+            _collection = database.GetCollection<Category>("Categories");
+        }
+
+  
+        public List<Category> GetAll()
+        {
+            return _collection.Find(_ => true).ToList();
+        }
+
+        public void Create(Category category)
+        {
+            _collection.InsertOne(category);
+        }
+
+
+        public void Delete(ObjectId id)
+        {
+            var filter = Builders<Category>.Filter.Eq(c => c.Id, id);
+            _collection.DeleteOne(filter);
+        }
+
+  
+        public Category GetById(ObjectId id)
+        {
+            var filter = Builders<Category>.Filter.Eq(c => c.Id, id);
+            return _collection.Find(filter).FirstOrDefault();
+        }
+    }
+}
